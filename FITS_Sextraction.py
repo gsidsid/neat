5b293d8dce15fdc9a5845f38114be805772dfb97
Siddharth Garimella <gsidsid@gmail.com>
2019-02-08 00:16:19 -0500

    creating a standardized catalog nomenclature

--- FITS_Sextraction.py
+++ FITS_Sextraction.py
@@ -33,7 +33,7 @@ os.system(
     " && sex ../" +
     file +
     " -PARAMETERS_NAME sex_outcols.txt -FILTER_NAME gauss_5.0_9x9_conv.txt -STARNNW_NAME default_nnw.txt -c wisesex_params.txt -MAG_ZEROPOINT 20.5 -SATUR_LEVEL 2500 -DETECT_THRESH 2 -GAIN 1.0e+20 -WEIGHT_GAIN N,N -CATALOG_NAME " +
-    light_id +
+    sample + '-' + light_id +
     "-sex-cat.txt -CHECKIMAGE_TYPE -OBJECTS,BACKGROUND -CHECKIMAGE_NAME obj.fits,bck.fits -DEBLEND_NTHRESH 32 -DEBLEND_MINCONT 0.0001 -BACK_SIZE 130")
 os.system(
     "cd " +
